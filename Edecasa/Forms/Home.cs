cfea49db775f44b89969c105bdc5aeb528acf789
Leonardo Lissone <leonardo.lissonez@gmail.com>
2021-04-28 16:21:53 -0300

    Home.cs organizada

--- Edecasa/Forms/Home.cs
+++ Edecasa/Forms/Home.cs
@@ -1,14 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
-using Edecasa.Properties;
 using Edecasa.Forms;
 using Edecasa.Models;
 using Edecasa.Controllers;
@@ -17,82 +10,13 @@ namespace Edecasa
 {
     public partial class Home : Form
     {
+        public static bool refresh;
+        public static int pedidoId;
+        public static double total;
+
         public Home()
         {
             InitializeComponent();
-        }
-        DBAccess objDBAccess = new DBAccess();
-        DataTable dtUsers = new DataTable();
-        //variaveis globais
-        public static bool refresh;
-        public static int pedidoId;
-        public static string pizza = "0", pizzametade = "0", bebida = "0", outro = "0";//para o form quantidade saber o item / =0 n tem registro
-        public static string registrar, datagora, pedido, limpar_array; //registrar adicionar valor na label de valor da home / refresh = dar um refresh na sacola
-        public static double valoritem, total; //calcular sacola de todos os items
-        //public static string[] idpedido = new string[200];
-        //public static string[] nomepedido = new string[200];
-        private void btnfechar_Click(object sender, EventArgs e)
-        {
-            if (pedidoId != 0)
-            {
-                MessageBox.Show("É necessário fechar o pedido para sair.", "Exclusão de Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                return;
-            }
-            this.Close();
-        }
-        private void moveSidePanel(Control btn)
-        {
-            panelsidehome.Top = btn.Top;
-            panelsidehome.Height = btn.Height;
-        }
-
-        private void btnhome_Click(object sender, EventArgs e)
-        {
-            UC_Home uch = new UC_Home();
-            AdicionarControlesParaPainel(uch);
-        }
-
-        private void btnlimpar_Click(object sender, EventArgs e)
-        {
-            if(DataGridViewItens.Rows.Count == 0)//SE SACOLA ESTIVER VAZIA
-            {
-                MessageBox.Show("Sacola de pedidos já está vazia!", "Exclusão de Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                return;
-            }
-
-            DialogResult dialog = MessageBox.Show("Você tem certeza que deseja limpar a sacola?", "Exclusão de Registro", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-            if (dialog != DialogResult.Yes)
-                return;
-
-            var itemController = new ItemController();
-            var ret = itemController.deleteByPedidoId(pedidoId);
-
-            if (!ret)
-            {
-                MessageBox.Show("Ocorreu um erro! Tente novamente.", "Exclusão de Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                return;
-            }
-
-            txtvalor.Text = "0";
-            total = 0;
-            refresh = true;
-        }
-
-        private void btnpedido_Click(object sender, EventArgs e)
-        {
-            panelsidehome.Visible = false;
-
-            UC_Home uch = new UC_Home();
-            AdicionarControlesParaPainel(uch);
-
-            CadastrarPedido abrirform = new CadastrarPedido();
-            abrirform.ShowDialog();
-        }
-
-        private void btnajustes_Click(object sender, EventArgs e)
-        {
-            Ajustes abrirform = new Ajustes();
-            abrirform.ShowDialog();
         }
 
         private void Home_Load(object sender, EventArgs e)
@@ -111,6 +35,43 @@ namespace Edecasa
             DataGridViewItens.Columns["Id"].Visible = false;
 
             refreshDataGrid();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            data.Text = "Data: " + DateTime.Now.ToShortDateString();
+            hora.Text = "Hora: " + DateTime.Now.ToLongTimeString();
+
+            if (refresh) //atualizar sacola
+            {
+                refreshDataGrid();
+                refresh = false;
+            }
+
+            if (pedidoId == 0)
+            {
+                btnpedido.Enabled = true;
+                txtsacola.Visible = false;
+                btncancelarpedido.Visible = false;
+                DataGridViewItens.Visible = false;
+                btnfinalizar.Visible = false;
+                btnlimpar.Visible = false;
+                txttotal.Visible = false;
+                txtrs.Visible = false;
+                txtvalor.Visible = false;
+            }
+            else
+            {
+                btnpedido.Enabled = false;
+                txtsacola.Visible = true;
+                btncancelarpedido.Visible = true;
+                DataGridViewItens.Visible = true;
+                btnfinalizar.Visible = true;
+                btnlimpar.Visible = true;
+                txttotal.Visible = true;
+                txtrs.Visible = true;
+                txtvalor.Visible = true;
+            }
         }
 
         private void refreshDataGrid()
@@ -160,6 +121,46 @@ namespace Edecasa
             txtvalor.Text = (total + itemValue).ToString();
         }
 
+        private void btnfinalizar_Click(object sender, EventArgs e)
+        {
+            //COLOCAR NOME DOS PEDIDOS NA DESCRICAO
+            //for(int i=0; i <= 199;i++)
+            //{
+            //    tbpedido.Text = tbpedido.Text + nomepedido[i];
+            //}
+            //pedido = tbpedido.Text;
+            //total = Convert.ToDouble(txtvalor.Text);
+            //CadastrarPedido abrirform = new CadastrarPedido();
+            //abrirform.ShowDialog();
+
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            if (DataGridViewItens.Rows.Count == 0)//SE SACOLA ESTIVER VAZIA
+            {
+                MessageBox.Show("Sacola de pedidos já está vazia!", "Exclusão de Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            DialogResult dialog = MessageBox.Show("Você tem certeza que deseja limpar a sacola?", "Exclusão de Registro", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialog != DialogResult.Yes)
+                return;
+
+            var itemController = new ItemController();
+            var ret = itemController.deleteByPedidoId(pedidoId);
+
+            if (!ret)
+            {
+                MessageBox.Show("Ocorreu um erro! Tente novamente.", "Exclusão de Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            txtvalor.Text = "0";
+            total = 0;
+            refresh = true;
+        }
+
         private void btncancelarpedido_Click(object sender, EventArgs e)
         {
             DialogResult dialog = MessageBox.Show("Você tem certeza que deseja cancelar este pedido?", "Exclusão de Registro", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
@@ -191,78 +192,8 @@ namespace Edecasa
 
             pedidoId = 0;
             txtvalor.Text = "0";
+
             MessageBox.Show("Pedido cancelado com sucesso!", "Exclusão de Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-
-        public void AdicionarControlesParaPainel(Control c)
-        {
-            c.Dock = DockStyle.Fill;
-            panelprincipal.Controls.Clear();
-            panelprincipal.Controls.Add(c);
-        }
-
-        private void btnoutros_Click(object sender, EventArgs e)
-        {
-            moveSidePanel(btnoutros);
-            panelsidehome.Visible = true;
-
-            UC_Outros uch = new UC_Outros(pedidoId);
-            AdicionarControlesParaPainel(uch);
-        }
-
-        private void btnpizzas_Click(object sender, EventArgs e)
-        {
-            moveSidePanel(btnpizzas);
-            panelsidehome.Visible = true;
-
-            UC_Pizzas uch = new UC_Pizzas(pedidoId);
-            AdicionarControlesParaPainel(uch);
-        }
-
-        private void btnbebidas_Click(object sender, EventArgs e)
-        {
-            moveSidePanel(btnbebidas);
-            panelsidehome.Visible = true;
-
-            UC_Bebidas uch = new UC_Bebidas(pedidoId);
-            AdicionarControlesParaPainel(uch);
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            data.Text = "Data: " + DateTime.Now.ToShortDateString();
-            hora.Text = "Hora: " + DateTime.Now.ToLongTimeString();
-
-            if(refresh) //atualizar sacola
-            {
-                refreshDataGrid();
-                refresh = false;
-            }
-
-            if (pedidoId == 0)
-            {
-                btnpedido.Enabled = true;
-                txtsacola.Visible = false;
-                btncancelarpedido.Visible = false;
-                DataGridViewItens.Visible = false;
-                btnfinalizar.Visible = false;
-                btnlimpar.Visible = false;
-                txttotal.Visible = false;
-                txtrs.Visible = false;
-                txtvalor.Visible = false;
-            }
-            else
-            {
-                btnpedido.Enabled = false;
-                txtsacola.Visible = true;
-                btncancelarpedido.Visible = true;
-                DataGridViewItens.Visible = true;
-                btnfinalizar.Visible = true;
-                btnlimpar.Visible = true;
-                txttotal.Visible = true;
-                txtrs.Visible = true;
-                txtvalor.Visible = true;
-            }
         }
 
         private void DataGridViewPedido_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
@@ -285,18 +216,78 @@ namespace Edecasa
 
             refresh = true;
         }
-        private void btnfinalizar_Click(object sender, EventArgs e)
+
+        private void btnfechar_Click(object sender, EventArgs e)
         {
-            //COLOCAR NOME DOS PEDIDOS NA DESCRICAO
-            //for(int i=0; i <= 199;i++)
-            //{
-            //    tbpedido.Text = tbpedido.Text + nomepedido[i];
-            //}
-            //pedido = tbpedido.Text;
-            //total = Convert.ToDouble(txtvalor.Text);
-            //CadastrarPedido abrirform = new CadastrarPedido();
-            //abrirform.ShowDialog();
-         
+            if (pedidoId != 0)
+            {
+                MessageBox.Show("É necessário fechar o pedido para sair.", "Exclusão de Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            this.Close();
+        }
+
+        private void btnhome_Click(object sender, EventArgs e)
+        {
+            UC_Home uch = new UC_Home();
+            AdicionarControlesParaPainel(uch);
+        }
+
+        private void btnpedido_Click(object sender, EventArgs e)
+        {
+            panelsidehome.Visible = false;
+
+            UC_Home uch = new UC_Home();
+            AdicionarControlesParaPainel(uch);
+
+            CadastrarPedido abrirform = new CadastrarPedido();
+            abrirform.ShowDialog();
+        }
+
+        private void btnpizzas_Click(object sender, EventArgs e)
+        {
+            moveSidePanel(btnpizzas);
+            panelsidehome.Visible = true;
+
+            UC_Pizzas uch = new UC_Pizzas(pedidoId);
+            AdicionarControlesParaPainel(uch);
+        }
+
+        private void btnbebidas_Click(object sender, EventArgs e)
+        {
+            moveSidePanel(btnbebidas);
+            panelsidehome.Visible = true;
+
+            UC_Bebidas uch = new UC_Bebidas(pedidoId);
+            AdicionarControlesParaPainel(uch);
+        }
+
+        private void btnoutros_Click(object sender, EventArgs e)
+        {
+            moveSidePanel(btnoutros);
+            panelsidehome.Visible = true;
+
+            UC_Outros uch = new UC_Outros(pedidoId);
+            AdicionarControlesParaPainel(uch);
+        }
+
+        private void btnajustes_Click(object sender, EventArgs e)
+        {
+            Ajustes abrirform = new Ajustes();
+            abrirform.ShowDialog();
+        }
+
+        public void AdicionarControlesParaPainel(Control c)
+        {
+            c.Dock = DockStyle.Fill;
+            panelprincipal.Controls.Clear();
+            panelprincipal.Controls.Add(c);
+        }
+
+        private void moveSidePanel(Control btn)
+        {
+            panelsidehome.Top = btn.Top;
+            panelsidehome.Height = btn.Height;
         }
     }
 }
